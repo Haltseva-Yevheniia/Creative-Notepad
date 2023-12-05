a39e74af1da764b233f666b40e41a2d4a0caa8c7
ViT <vitaliy.onda@gmail.com>
2023-12-05 22:02:05 +0200

    vit_first commit

--- lib/page/first_screen.dart
+++ lib/page/first_screen.dart
@@ -9,6 +9,9 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+
+  List<String> dataList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
