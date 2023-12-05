a4db4230a9db63936241e5da8c07a4eca5ece6d1
Пользователь <dorosenkooleg006@gmail.com>
2023-12-05 23:33:47 +0200

    added appdar and body

--- lib/page/first_screen.dart
+++ lib/page/first_screen.dart
@@ -9,9 +9,45 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+
+  List<String> notes = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: const Text(
+         'Notepad',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            letterSpacing: 3,
+          ),
+        ),
+          actions: const [
+            Icon(Icons.ac_unit, size: 30.0),
+            Icon(Icons.account_balance_sharp, size: 30.0),
+          ],
+         ),
+
+      body: notes.isEmpty ? const Center(
+          child: Text(
+                'Ваш нотаток пустий, додайте замітку',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.w500,
+              ),
+             ),
+            )
+          : ListView.builder(
+           itemCount: notes.length,
+           itemBuilder: (context, index){
+             return const ListTile(
+
+            );
+          },
+      ),
       floatingActionButton: FloatingActionButton(onPressed: (){}),
     );
   }
